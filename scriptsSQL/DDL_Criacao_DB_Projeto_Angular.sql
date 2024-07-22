80edfcf6c0e0093f69fb05e2e9dcdb54daad6a28
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-07-22 12:15:11 -0300

    feat: Adiciona script de criação de de base de dados e README
    O readme passa referecia da documentação (gerado automaticamente) e um guia de configurações iniciais do rpojeto

---
+++ scriptsSQL/DDL_Criacao_DB_Projeto_Angular.sql
@@ -0,0 +1,12 @@
+-- SCRIPT PARA CRIAÇÃO DA BASE DE DADOS
+--
+-- Sobre: Este script cria a base de dados para o funcionamento do projeto
+-- de Spring com Angular
+--
+-- Author: Rafael Berto Pereira
+-- Data Criação: 22/07/2024
+-- Última alteração: 22/07/2024
+--
+-- Histórico: > Criação do script
+
+CREATE DATABASE IF NOT EXISTS projeto_angular_spring
