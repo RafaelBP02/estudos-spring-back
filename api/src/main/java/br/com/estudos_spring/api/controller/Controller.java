430a178d21e915e1f3da1830fde39ba111d43fd8
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-07-22 12:44:59 -0300

    feat: Cria um teste de comunicação com o banco de dados

--- api/src/main/java/br/com/estudos_spring/api/controller/Controller.java
+++ api/src/main/java/br/com/estudos_spring/api/controller/Controller.java
@@ -1,5 +1,13 @@
 package br.com.estudos_spring.api.controller;
 
-public class Controller {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class Controller {
+    
+    @GetMapping("/")
+    public String teste(){
+        return "Hello World";
+    }
 }
