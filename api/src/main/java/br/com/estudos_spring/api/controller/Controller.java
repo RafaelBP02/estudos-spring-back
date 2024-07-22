4bc2c844c590ba05d6f76fe728019e043c639a99
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-07-22 14:47:45 -0300

    feat: adiciona requisição para deletar um cliente por ID

--- api/src/main/java/br/com/estudos_spring/api/controller/Controller.java
+++ api/src/main/java/br/com/estudos_spring/api/controller/Controller.java
@@ -1,7 +1,9 @@
 package br.com.estudos_spring.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +23,12 @@ public class Controller {
     }
 
     @GetMapping("/")
-    public String teste(){
-        return "Hello World";
+    public Iterable<Client> selectClients(){
+        return action.findAll();
+    }
+
+    @DeleteMapping("/{codigo}")
+    public void deleteClient(@PathVariable Long codigo){
+        action.deleteById(codigo);
     }
 }
