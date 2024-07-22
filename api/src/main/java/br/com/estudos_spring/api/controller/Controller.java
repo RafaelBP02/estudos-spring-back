3e25bd0138afb7027b659246d91c628b3fd817b9
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-07-22 14:30:18 -0300

    feat: Adiciona cadastro de clientes
    Foram realizados testes manuais para verificar o funcionamento dessa requisição post

--- api/src/main/java/br/com/estudos_spring/api/controller/Controller.java
+++ api/src/main/java/br/com/estudos_spring/api/controller/Controller.java
@@ -1,11 +1,25 @@
 package br.com.estudos_spring.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.estudos_spring.api.model.Client;
+import br.com.estudos_spring.api.repo.Repo;
 
 @RestController
 public class Controller {
     
+    @Autowired
+    private Repo action;
+
+    @PostMapping("/")
+    public Client register(@RequestBody Client c){
+        return action.save(c);
+    }
+
     @GetMapping("/")
     public String teste(){
         return "Hello World";
