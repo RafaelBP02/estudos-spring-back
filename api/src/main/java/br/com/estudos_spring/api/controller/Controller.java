4a383a9ffb0971c0b06e19bc2e7ba098bb1f5406
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-07-23 11:46:47 -0300

    feat: Adiciona requisição de put

--- api/src/main/java/br/com/estudos_spring/api/controller/Controller.java
+++ api/src/main/java/br/com/estudos_spring/api/controller/Controller.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,11 @@ public class Controller {
 
     @PostMapping("/")
     public Client register(@RequestBody Client c){
+        return action.save(c);
+    }
+
+    @PutMapping("/")
+    public Client edit(@RequestBody Client c){
         return action.save(c);
     }
 
